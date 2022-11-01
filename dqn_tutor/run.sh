8bb45ce979280b5a06a55228ae2365345bd311e8
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2022-11-01 14:33:19 +0800

    m

--- dqn_tutor/run.sh
+++ dqn_tutor/run.sh
@@ -4,19 +4,24 @@
 script=$1
 
 if [ -z "$script" ]; then
-    echo "exectute dqn.py"
+    # echo "exectute dqn.py"
     script="dqn.py"
 fi
 
 set -e 
 
-# docker run --name gym --rm -it -v `pwd`:/opt/work/ \
-#     -e DISPLAY=$DISPLAY \
-#     mebusy/gym $script
+# whether os is dawwin
+if [ "$(uname)" == "Darwin" ]; then
+    docker run --name gym --rm -it -v `pwd`:/opt/work/ \
+        -e DISPLAY=$DISPLAY \
+        mebusy/gym $script
+else
+    docker run  --rm -it -v `pwd`:/opt/work/  --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864  \
+        -e DISPLAY='10.192.89.36:0' \
+        mebusy/nv_torch_gym  python $script
+fi
 
 
-docker run  --rm -it -v `pwd`:/opt/work/  --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864  \
-    -e DISPLAY='10.192.89.36:0' \
-    mebusy/nv_torch_gym  python $script
+
 
 
