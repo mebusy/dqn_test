910242b70ebb857e8aa1c3a3a31e7e6075083598
qibinyi <qibinyi@Qis-Mac-mini-6.local>
2022-11-10 23:08:56 +0800

    m

---
+++ fctorch/dataload.py
@@ -0,0 +1,32 @@
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+NUM_TRAIN = 49000
+
+# The torchvision.transforms package provides tools for preprocessing data
+# and for performing data augmentation; here we set up a transform to
+# preprocess the data by subtracting the mean RGB value and dividing by the
+# standard deviation of each RGB value; we've hardcoded the mean and std.
+transform = T.Compose(
+    [T.ToTensor(), T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
+)
+
+
+cifar10_train = dset.CIFAR10(
+    "./datasets", train=True, download=True, transform=transform
+)
+loader_train = DataLoader(
+    cifar10_train, batch_size=64, sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN))
+)
+
+# **acutally same as cifar10_train**
+cifar10_val = dset.CIFAR10("./datasets", train=True, download=True, transform=transform)
+# **but be different on subset**
+loader_val = DataLoader(
+    cifar10_val,
+    batch_size=64,
+    sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, 50000)),
+)
---
+++ fctorch/torchFC.py
@@ -0,0 +1,113 @@
+#!python3
+import torch
+import torch.nn as nn
+
+import torch.nn.functional as F
+import torch.optim as optim
+
+# import numpy as np
+from dataload import loader_train, loader_val
+
+dtype = torch.float32  # we will be using float throughout this tutorial
+
+USE_GPU = True
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+# Constant to control how frequently we print train loss
+print_every = 100
+
+print("using device:", device)
+
+
+def flatten(x):
+    N = x.shape[0]  # read in N, C, H, W
+    # "flatten" the C * H * W values into a single vector per image
+    return x.view(N, -1)
+
+
+# We need to wrap `flatten` function in a module in order to stack it
+# in nn.Sequential
+class Flatten(nn.Module):
+    def forward(self, x):
+        return flatten(x)
+
+
+def check_accuracy_part34(loader, model):
+    if loader.dataset.train:
+        print("Checking accuracy on validation set")
+    else:
+        print("Checking accuracy on test set")
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print("Got %d / %d correct (%.2f)" % (num_correct, num_samples, 100 * acc))
+
+
+def train_part34(model, optimizer, epochs=1):
+    """
+    Train a model on CIFAR-10 using the PyTorch Module API.
+
+    Inputs:
+    - model: A PyTorch Module giving the model to train.
+    - optimizer: An Optimizer object we will use to train the model
+    - epochs: (Optional) A Python integer giving the number of epochs to train for
+
+    Returns: Nothing, but prints model accuracies during training.
+    """
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for e in range(epochs):
+        for t, (x, y) in enumerate(loader_train):
+            model.train()  # put model to training mode
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+            loss = F.cross_entropy(scores, y)
+
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+            optimizer.zero_grad()
+
+            # This is the backwards pass: compute the gradient of the loss with
+            # respect to each  parameter of the model.
+            loss.backward()
+
+            # Actually update the parameters of the model using the gradients
+            # computed by the backwards pass.
+            optimizer.step()
+
+            if t % print_every == 0:
+                print("Iteration %d, loss = %.4f" % (t, loss.item()))
+                check_accuracy_part34(loader_val, model)
+                print()
+
+
+# --------------------------- model ----------------------
+
+hidden_layer_size = 4000
+learning_rate = 1e-2
+
+model = nn.Sequential(
+    Flatten(),
+    nn.Linear(3 * 32 * 32, hidden_layer_size),
+    nn.ReLU(),
+    nn.Linear(hidden_layer_size, 10),
+)
+
+# you can use Nesterov momentum in optim.SGD
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, nesterov=True)
+
+if __name__ == "__main__":
+    train_part34(model, optimizer)
