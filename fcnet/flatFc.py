32b2b8165a43c1bcbd6c9938aaa0bf366a47d938
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2022-11-10 17:49:22 +0800

    m

---
+++ fcnet/flatFc.py
@@ -0,0 +1,369 @@
+#!python3
+import numpy as np
+
+
+# -------------------------- forward/backward-------------------------------------------
+
+
+def affine_forward(x, w, b):
+    out = x.reshape(x.shape[0], -1).dot(w) + b
+    cache = (x, w, b)
+    return out, cache
+
+
+def affine_backward(dout, cache):
+    x, w, b = cache
+    dx = dout.dot(w.T).reshape(x.shape)
+    dw = x.reshape(x.shape[0], -1).T.dot(dout)
+    db = np.sum(dout, axis=0)
+    return dx, dw, db
+
+
+def relu_forward(x):
+    out = np.maximum(0, x)
+    cache = x
+    return out, cache
+
+
+def relu_backward(dout, cache):
+    x = cache
+    dx = np.where(x > 0, dout, 0)
+    return dx
+
+
+def affine_relu_forward(x, w, b):
+    a, fc_cache = affine_forward(x, w, b)
+    out, relu_cache = relu_forward(a)
+    cache = (fc_cache, relu_cache)
+    return out, cache
+
+
+def affine_relu_backward(dout, cache):
+    fc_cache, relu_cache = cache
+    da = relu_backward(dout, relu_cache)
+    dx, dw, db = affine_backward(da, fc_cache)
+    return dx, dw, db
+
+
+# -------------------------- loss function -------------------------------------------
+
+
+def softmax_loss(x, y):
+    probs = np.exp(x - np.max(x, axis=1, keepdims=True))
+    probs /= np.sum(probs, axis=1, keepdims=True)
+    N = x.shape[0]
+    loss = -np.sum(np.log(probs[np.arange(N), y])) / N
+    dx = probs.copy()
+    dx[np.arange(N), y] -= 1
+    dx /= N
+    return loss, dx
+
+
+# -------------------------- optim -------------------------------------------
+
+
+def sgd(w, dw, config=None):
+    if config is None:
+        config = {}
+    config.setdefault("learning_rate", 1e-2)
+
+    w -= config["learning_rate"] * dw
+    return w, config
+
+
+# -------------------------- FC Net -------------------------------------------
+
+
+class TwoLayerNet(object):
+    """
+    Input dimension: D, hidden dimension  H,  C classes.
+    Architecure:  affine - relu - affine - softmax.
+    """
+
+    def __init__(
+        self,
+        input_dim=3 * 32 * 32,
+        hidden_dim=100,
+        num_classes=10,
+        weight_scale=1e-3,
+        reg=0.0,
+    ):
+        """
+        Initialize a new network.
+
+        Inputs:
+        - input_dim: An integer giving the size of the input
+        - hidden_dim: An integer giving the size of the hidden layer
+        - num_classes: An integer giving the number of classes to classify
+        - weight_scale: Scalar giving the standard deviation for random
+          initialization of the weights.
+        - reg: Scalar giving L2 regularization strength.
+        """
+        self.params = {}
+        self.reg = reg
+
+        # std = weight_scale.  PS. NOT variance
+        self.params["W1"] = np.random.randn(input_dim, hidden_dim) * weight_scale
+        self.params["W2"] = np.random.randn(hidden_dim, num_classes) * weight_scale
+
+        self.params["b1"] = np.zeros(hidden_dim)
+        self.params["b2"] = np.zeros(num_classes)
+
+        pass
+
+    def loss(self, X, y=None):
+        """
+        Compute loss and gradient for a minibatch of data.
+
+        Inputs:
+        - X: Array of input data of shape (N, d_1, ..., d_k)
+        - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+        Returns:
+        If y is None, then run a test-time forward pass of the model and return:
+        - scores: Array of shape (N, C) giving classification scores, where
+          scores[i, c] is the classification score for X[i] and class c.
+
+        If y is not None, then run a training-time forward and backward pass and
+        return a tuple of:
+        - loss: Scalar value giving the loss
+        - grads: Dictionary with the same keys as self.params, mapping parameter
+          names to gradients of the loss with respect to those parameters.
+        """
+        scores = None
+
+        _scores1, cache1 = affine_relu_forward(X, self.params["W1"], self.params["b1"])
+        scores, cache2 = affine_forward(_scores1, self.params["W2"], self.params["b2"])
+
+        # If y is None then we are in test mode so just return scores
+        if y is None:
+            return scores
+
+        loss, grads = 0, {}
+
+        W1 = self.params["W1"]
+        W2 = self.params["W2"]
+
+        loss, smx_grad = softmax_loss(scores, y)  # loss of score
+        # + loss of regularization
+        # **loss won't be actually used in calculating gradients**
+        loss += 0.5 * self.reg * ((W1 * W1).sum() + (W2 * W2).sum())
+
+        dx2, dw2, db2 = affine_backward(smx_grad, cache2)
+        grads["W2"] = dw2 + self.reg * W2
+        grads["b2"] = db2
+
+        dx1, dw1, db1 = affine_relu_backward(dx2, cache1)
+        grads["W1"] = dw1 + self.reg * W1
+        grads["b1"] = db1
+
+        return loss, grads
+
+
+# -------------------------- Solver -------------------------------------------
+
+
+class Solver(object):
+    def __init__(self, model, data, **kwargs):
+        self.model = model
+        self.X_train = data["X_train"]
+        self.y_train = data["y_train"]
+        self.X_val = data["X_val"]
+        self.y_val = data["y_val"]
+
+        # Unpack keyword arguments
+        self.optim_config = kwargs.pop("optim_config", {})
+        self.lr_decay = kwargs.pop("lr_decay", 1.0)
+        self.batch_size = kwargs.pop("batch_size", 100)
+        self.num_epochs = kwargs.pop("num_epochs", 10)
+        self.num_train_samples = kwargs.pop("num_train_samples", 1000)
+        self.num_val_samples = kwargs.pop("num_val_samples", None)
+
+        self.checkpoint_name = kwargs.pop("checkpoint_name", None)
+        self.print_every = kwargs.pop("print_every", 10)
+        self.verbose = kwargs.pop("verbose", True)
+
+        # Throw an error if there are extra keyword arguments
+        if len(kwargs) > 0:
+            extra = ", ".join('"%s"' % k for k in list(kwargs.keys()))
+            raise ValueError("Unrecognized arguments %s" % extra)
+
+        # Make sure the update rule exists, then replace the string
+        # name with the actual function
+        self.update_rule = sgd
+
+        self._reset()
+
+    def _reset(self):
+        """
+        Set up some book-keeping variables for optimization. Don't call this
+        manually.
+        """
+        # Set up some variables for book-keeping
+        self.epoch = 0
+        self.best_val_acc = 0
+        self.best_params = {}
+        self.loss_history = []
+        self.train_acc_history = []
+        self.val_acc_history = []
+
+        # Make a deep copy of the optim_config for each parameter
+        self.optim_configs = {}
+        for p in self.model.params:
+            d = {k: v for k, v in self.optim_config.items()}
+            self.optim_configs[p] = d
+
+    def _step(self):
+        """
+        Make a single gradient update. This is called by train() and should not
+        be called manually.
+        """
+        # Make a minibatch of training data
+        num_train = self.X_train.shape[0]
+        batch_mask = np.random.choice(num_train, self.batch_size)
+        X_batch = self.X_train[batch_mask]
+        y_batch = self.y_train[batch_mask]
+
+        # Compute loss and gradient
+        loss, grads = self.model.loss(X_batch, y_batch)
+        self.loss_history.append(loss)
+
+        # Perform a parameter update
+        for p, w in self.model.params.items():
+            dw = grads[p]
+            config = self.optim_configs[p]
+            next_w, next_config = self.update_rule(w, dw, config)
+            self.model.params[p] = next_w
+            self.optim_configs[p] = next_config
+
+    def _save_checkpoint(self):
+        if self.checkpoint_name is None:
+            return
+        checkpoint = {
+            "model": self.model,
+            "update_rule": self.update_rule,
+            "lr_decay": self.lr_decay,
+            "optim_config": self.optim_config,
+            "batch_size": self.batch_size,
+            "num_train_samples": self.num_train_samples,
+            "num_val_samples": self.num_val_samples,
+            "epoch": self.epoch,
+            "loss_history": self.loss_history,
+            "train_acc_history": self.train_acc_history,
+            "val_acc_history": self.val_acc_history,
+        }
+        filename = "%s_epoch_%d.pkl" % (self.checkpoint_name, self.epoch)
+        if self.verbose:
+            print('Saving checkpoint to "%s"' % filename)
+        with open(filename, "wb") as f:
+            import pickle as pickle
+
+            pickle.dump(checkpoint, f)
+
+    def check_accuracy(self, X, y, num_samples=None, batch_size=100):
+        # Maybe subsample the data
+        N = X.shape[0]
+        if num_samples is not None and N > num_samples:
+            mask = np.random.choice(N, num_samples)
+            N = num_samples
+            X = X[mask]
+            y = y[mask]
+
+        # Compute predictions in batches
+        num_batches = N // batch_size
+        if N % batch_size != 0:
+            num_batches += 1
+        y_pred = []
+        for i in range(num_batches):
+            start = i * batch_size
+            end = (i + 1) * batch_size
+            scores = self.model.loss(X[start:end])
+            y_pred.append(np.argmax(scores, axis=1))
+        y_pred = np.hstack(y_pred)
+        acc = np.mean(y_pred == y)
+
+        return acc
+
+    def train(self):
+        """
+        Run optimization to train the model.
+        """
+        num_train = self.X_train.shape[0]
+        iterations_per_epoch = max(num_train // self.batch_size, 1)
+        num_iterations = self.num_epochs * iterations_per_epoch
+
+        for t in range(num_iterations):
+            self._step()
+
+            # Maybe print training loss
+            if self.verbose and t % self.print_every == 0:
+                print(
+                    "(Iteration %d / %d) loss: %f"
+                    % (t + 1, num_iterations, self.loss_history[-1])
+                )
+
+            # At the end of every epoch, increment the epoch counter and decay
+            # the learning rate.
+            epoch_end = (t + 1) % iterations_per_epoch == 0
+            if epoch_end:
+                self.epoch += 1
+                for k in self.optim_configs:
+                    self.optim_configs[k]["learning_rate"] *= self.lr_decay
+
+            # Check train and val accuracy on the first iteration, the last
+            # iteration, and at the end of each epoch.
+            first_it = t == 0
+            last_it = t == num_iterations - 1
+            if first_it or last_it or epoch_end:
+                train_acc = self.check_accuracy(
+                    self.X_train, self.y_train, num_samples=self.num_train_samples
+                )
+                val_acc = self.check_accuracy(
+                    self.X_val, self.y_val, num_samples=self.num_val_samples
+                )
+                self.train_acc_history.append(train_acc)
+                self.val_acc_history.append(val_acc)
+                self._save_checkpoint()
+
+                if self.verbose:
+                    print(
+                        "(Epoch %d / %d) train acc: %f; val_acc: %f"
+                        % (self.epoch, self.num_epochs, train_acc, val_acc)
+                    )
+
+                # Keep track of the best model
+                if val_acc > self.best_val_acc:
+                    self.best_val_acc = val_acc
+                    self.best_params = {}
+                    for k, v in self.model.params.items():
+                        self.best_params[k] = v.copy()
+
+        # At the end of training swap the best params into the model
+        self.model.params = self.best_params
+
+
+# -------------------------- train -------------------------------------------
+
+from data_utils import get_CIFAR10_data
+
+data = get_CIFAR10_data()
+for k, v in list(data.items()):
+    print(("%s: " % k, v.shape))
+
+model = TwoLayerNet(
+    input_dim=32 * 32 * 3, hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=3e-1
+)
+
+solver = Solver(
+    model,
+    data,
+    optim_config={
+        "learning_rate": 1e-3,
+    },
+    lr_decay=0.95,
+    num_epochs=10,
+    batch_size=100,
+    print_every=100,
+)
+
+solver.train()
